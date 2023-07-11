ad004808b1c9812c02ba6f98d3f7ea12d6eed172
mengistuabebe06 <mengistuabebe06@gmail.com>
2023-07-11 06:11:10 +0300

    first commit

---
+++ ticTac.js
@@ -0,0 +1,8 @@
+const cellElements = document.querySelectorAll('[data-cell]')
+
+cellElements.forEach(cell =>{
+    cell.addEventListener('click', handleClick, {once: true})
+})
+function handleClick(e){
+console.log("click cell");
+}
